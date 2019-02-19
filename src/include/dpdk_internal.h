648137528b24606d8f1c64f90c4cc01e3bea2962
Jörg Thalheim <joerg@thalheim.io>
2019-02-19 12:04:37 +0100

    dpdk: add missing files

---
+++ src/include/dpdk_internal.h
@@ -0,0 +1,10 @@
+#include <rte_config.h>
+#include <eal_internal_cfg.h>
+
+struct dpdk_context {
+    struct rte_eth_devices *devices;
+    struct rte_config *config;
+    struct lcore_config *lcore_config;
+    struct internal_config *internal_config;
+};
+void dpdk_init_array();
