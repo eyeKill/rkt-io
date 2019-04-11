760229a68cb6e48d3e4365a2fa2ce019254f6c80
Jörg Thalheim <joerg@thalheim.io>
2019-04-11 17:20:45 +0200

    nix/run-image: add option generate flamegraphs

--- apps/nix/run-image.nix
+++ apps/nix/run-image.nix
@@ -1,12 +1,14 @@
-{ runtimeShell, pkgsMusl, pwndbg, writeScript, buildImage }:
+{ runtimeShell, lib, flamegraph, pkgsMusl, writeScript, buildImage }:
 
-{ pkg, command, extraFiles ? {}, debugSymbols ? true, diskSize ? "10G" }:
+{ pkg, command, extraFiles ? {}, extraCommands ? "", debugSymbols ? true, diskSize ? "10G" }:
 
 let
-  image = buildImage { inherit pkg extraFiles debugSymbols diskSize; };
+  image = buildImage { inherit pkg extraFiles extraCommands debugSymbols diskSize; };
 in writeScript "run-lkl" ''
   #!${runtimeShell}
   set -eu -o pipefail -x
+
+  export PATH=${lib.makeBinPath [ flamegraph ]}:$PATH
 
   if [[ $# -eq 0 ]]; then
     cmd="${toString command}"
@@ -17,26 +19,40 @@ in writeScript "run-lkl" ''
 
   tmppath=$(mktemp -d)
   cleanup() {
-    if [[ -n "$pid" ]]; then
+    if [[ -n "''${pid:-}" ]]; then
       kill $pid
     fi
+
+    if [[ ''${SGXLKL_ENABLE_FLAMEGRAPH:-} ]]; then
+      flamegraph="${image.pkg.name}-$(date +%s).svg"
+
+      perf script -i $tmppath/perf.data \
+        | stackcollapse-perf.pl \
+        | flamegraph.pl > $flamegraph
+      echo $flamegraph
+    fi
+
     rm -rf "$tmppath";
   }
-  trap cleanup EXIT SIGINT SIGQUIT ERR
+  trap cleanup EXIT
 
   DEBUGGER=
   if [[ -n ''${SGXLKL_ENABLE_GDB:-} ]]; then
     DEBUGGER="sgx-lkl-gdb"
-    if [[ -n ''${SGXLKL_ENABLE_PWNDBG:-} ]]; then
-      DEBUGGER="$DEBUGGER -x ${pwndbg}/share/pwndbg/gdbinit.py"
-    fi
     DEBUGGER="$DEBUGGER --args"
   elif [[ -n ''${SGXLKL_ENABLE_STRACE:-} ]]; then
     DEBUGGER="strace"
+  elif [[ -n ''${SGXLKL_ENABLE_FLAMEGRAPH:-} ]]; then
+    DEBUGGER="perf record -o $tmppath/perf.data -g -F99 --"
   fi
 
   install -m660 ${image} $tmppath/fs.img
-  TMPDIR=/tmp $DEBUGGER sgx-lkl-run $tmppath/fs.img ${image.pkg}/$cmd "$@" &
-  pid=$!
-  wait $pid
+
+  if [[ -n "$DEBUGGER" ]]; then
+    TMPDIR=/tmp $DEBUGGER sgx-lkl-run $tmppath/fs.img ${image.pkg}/$cmd "$@"
+  else
+    TMPDIR=/tmp sgx-lkl-run $tmppath/fs.img ${image.pkg}/$cmd "$@" &
+    pid=$!
+    wait $pid
+  fi
 ''
