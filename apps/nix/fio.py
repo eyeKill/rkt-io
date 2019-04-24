0c04b901db7bc3432fce3cbc4cf78be6a9800b89
Jörg Thalheim <joerg@thalheim.io>
2019-04-24 11:46:32 +0200

    fio: get all three benchmarks working

--- apps/nix/fio.py
+++ apps/nix/fio.py
@@ -1,15 +1,16 @@
+import getpass
+import json
 import os
 import subprocess
 import tempfile
-import json
 import time
-import getpass
 from collections import defaultdict
 from enum import Enum
 from typing import Any, DefaultDict, Dict, List, Union
 
 import pandas as pd
-from helpers import NOW, ROOT, Settings, create_settings, nix_build, run, spawn, Chdir
+from helpers import (NOW, ROOT, Chdir, Settings, create_settings, nix_build,
+                     run, spawn)
 
 
 class StorageKind(Enum):
@@ -65,14 +66,14 @@ class Storage:
                     "config",
                 ]
             )
+        elif kind == StorageKind.LKL:
+            subprocess.run(["sudo", "chown", getpass.getuser(), dev])
+
         return Mount(kind, dev)
 
 
-def create_image() -> None:
-    pass
-
 def percentile(idx: int, run_total: List[int]) -> float:
-    total = run_total[len(run_total)-1]
+    total = run_total[len(run_total) - 1]
     if total == 0:
         return 0
 
@@ -87,25 +88,30 @@ def more_lines(indices: Dict[str, int], bins: Dict[str, List[List[Any]]]) -> boo
     return False
 
 
-def parse_json_plus(jsondata: Dict[str, Any], system: str) -> DefaultDict[str, List]:
-    data: DefaultDict[str, List] = defaultdict(list)
-    for jobnum in range(0, len(jsondata['jobs'])):
+def parse_json_plus(
+    jsondata: Dict[str, Any], system: str, stats: Dict[str, List]
+) -> None:
+    for jobnum in range(0, len(jsondata["jobs"])):
         bins = {}
         run_total = {}
-        operation_set = set(['read', 'write', 'trim'])
+        operation_set = set(["read", "write", "trim"])
 
         prev_operation = None
         for operation in operation_set:
-            if 'bins' in jsondata['jobs'][jobnum][operation]['clat_ns']:
-                bins_loc = 'clat_ns'
-            elif 'bins' in jsondata['jobs'][jobnum][operation]['lat_ns']:
-                bins_loc = 'lat_ns'
+            if "bins" in jsondata["jobs"][jobnum][operation]["clat_ns"]:
+                bins_loc = "clat_ns"
+            elif "bins" in jsondata["jobs"][jobnum][operation]["lat_ns"]:
+                bins_loc = "lat_ns"
             else:
-                raise RuntimeError("Latency bins not found. "
-                                   "Are you sure you are using json+ output?")
+                raise RuntimeError(
+                    "Latency bins not found. "
+                    "Are you sure you are using json+ output?"
+                )
 
             bin = []
-            for key, value in jsondata['jobs'][jobnum][operation][bins_loc]['bins'].items():
+            for key, value in jsondata["jobs"][jobnum][operation][bins_loc][
+                "bins"
+            ].items():
                 bin.append([int(key), value])
             bins[operation] = bin
             bins[operation] = sorted(bins[operation], key=lambda bin: bin[0])
@@ -114,69 +120,73 @@ def parse_json_plus(jsondata: Dict[str, Any], system: str) -> DefaultDict[str, L
             if len(bins[operation]) > 0:
                 run_total[operation][0] = bins[operation][0][1]
                 for x in range(1, len(bins[operation])):
-                    run_total[operation][x] = run_total[operation][x-1] + \
-                        bins[operation][x][1]
+                    run_total[operation][x] = (
+                        run_total[operation][x - 1] + bins[operation][x][1]
+                    )
 
         # Have a counter for each operation
         # In each round, pick the shortest remaining duration
         # and output a line with any values for that duration
         indices = {x: 0 for x in operation_set}
         while more_lines(indices, bins):
-            min_lat = 17112760320
+            min_lat = 17_112_760_320
             for operation in operation_set:
                 if indices[operation] < len(bins[operation]):
                     min_lat = min(bins[operation][indices[operation]][0], min_lat)
 
-            data["job"].append(jobnum)
-            data["system"].append(system)
-            data[f"{bins_loc}ec"].append(min_lat)
+            stats["job"].append(jobnum)
+            stats["system"].append(system)
+            stats[f"{bins_loc}ec"].append(min_lat)
 
             for operation in operation_set:
-                if indices[operation] < len(bins[operation]) and \
-                   min_lat == bins[operation][indices[operation]][0]:
+                if (
+                    indices[operation] < len(bins[operation])
+                    and min_lat == bins[operation][indices[operation]][0]
+                ):
                     count = bins[operation][indices[operation]][1]
                     cumulative = run_total[operation][indices[operation]]
                     ptile = percentile(indices[operation], run_total[operation])
-                    data[f"{operation}_count"].append(count)
-                    data[f"{operation}_cumulative"].append(cumulative)
-                    data[f"{operation}_percentile"].append(ptile)
+                    stats[f"{operation}_count"].append(count)
+                    stats[f"{operation}_cumulative"].append(cumulative)
+                    stats[f"{operation}_percentile"].append(ptile)
                     indices[operation] += 1
                 else:
-                    data[f"{operation}_count"].append(None)
-                    data[f"{operation}_cumulative"].append(None)
-                    data[f"{operation}_percentile"].append(None)
-    return data
+                    stats[f"{operation}_count"].append(None)
+                    stats[f"{operation}_cumulative"].append(None)
+                    stats[f"{operation}_percentile"].append(None)
 
 
-def benchmark_fio(storage: Storage, system: str, attr: str, directory: str):
+def benchmark_fio(
+    storage: Storage, system: str, attr: str, directory: str, stats: Dict[str, List], extra_env: Dict[str,str]={}
+):
     flamegraph = f"fio-{NOW}.svg"
     print(flamegraph)
 
-    env = dict(FLAMEGRAPH_FILENAME=flamegraph,
-               SGXLKL_ENABLE_FLAMEGRAPH="1",
-               SGXLKL_CWD=directory)
+    env = dict(
+        FLAMEGRAPH_FILENAME=flamegraph,
+        SGXLKL_ENABLE_FLAMEGRAPH="1",
+        SGXLKL_CWD=directory,
+    )
+    env.update(extra_env)
     fio = nix_build(attr)
     proc = run([fio], extra_env=env)
     jsondata = json.loads(proc.stdout)
-    data = parse_json_plus(jsondata, system)
-    csv = f"fio-{NOW}.tsv"
-    print(csv)
-    pd.DataFrame(data).to_csv(csv, index=False)
+    parse_json_plus(jsondata, system, stats)
 
 
-def benchmark_native(storage: Storage, stats: Dict[str, List[int]]) -> None:
+def benchmark_native(storage: Storage, stats: Dict[str, List]) -> None:
     with storage.setup(StorageKind.NATIVE) as mnt:
-        benchmark_fio(storage, "native", "fio-native", mnt)
+        benchmark_fio(storage, "native", "fio-native", mnt, stats)
 
 
-def benchmark_sgx_lkl(storage: Storage, stats: Dict[str, List[int]]) -> None:
+def benchmark_sgx_lkl(storage: Storage, stats: Dict[str, List]) -> None:
     storage.setup(StorageKind.LKL)
-    benchmark_fio(storage, "sgx-lkl", "fio", "/mnt/vdb")
+    benchmark_fio(storage, "sgx-lkl", "fio", "/mnt/nvme", stats, extra_env=dict(SGXLKL_HDS="/dev/nvme0n1:/mnt/nvme"))
 
 
-def benchmark_sgx_io(storage: Storage, stats: Dict[str, List[int]]) -> None:
+def benchmark_sgx_io(storage: Storage, stats: Dict[str, List]) -> None:
     storage.setup(StorageKind.SPDK)
-    benchmark_fio(storage, "sgx-io", "fio", "/mnt/vdb")
+    benchmark_fio(storage, "sgx-io", "fio", "/mnt/vdb", stats)
 
 
 def main() -> None:
@@ -187,8 +197,8 @@ def main() -> None:
     storage = Storage(settings)
 
     benchmark_native(storage, stats)
-    #benchmark_sgx_lkl(storage, stats)
-    #benchmark_sgx_io(storage, stats)
+    benchmark_sgx_lkl(storage, stats)
+    benchmark_sgx_io(storage, stats)
 
     csv = f"fio-{NOW}.tsv"
     print(csv)
