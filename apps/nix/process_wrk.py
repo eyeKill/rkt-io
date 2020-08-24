017f21ff934de873a4ad4cc7cb6ee21c94b49502
harshanavkis <harshanavkis@gmail.com>
2020-08-24 20:53:47 +0000

    Benchmark: Nginx proper csv col names, mib

--- apps/nix/process_wrk.py
+++ apps/nix/process_wrk.py
@@ -1,8 +1,21 @@
 import re
 from typing import Any, Dict
 
+def wrk_csv_cols() -> str:
+    return """Latency average [ms],Latency stdev [ms],Latency max[ms],Requests average,Requests stdev,Requests max,Total requests,Total duration,read,err_connect,err_read,err_write,err_timeout,Req/Sec,Transfer/sec [MB/s]"""
 
-def get_bytes(size_str: str) -> Any:
+def wrk_data(wrk_output: Dict[str, Any]) -> str:
+    return str(wrk_output.get('lat_avg')) + ',' + str(wrk_output.get('lat_stdev')) + ',' + str(
+        wrk_output.get('lat_max')) + ',' + str(wrk_output.get('req_avg')) + ',' + str(
+        wrk_output.get('req_stdev')) + ',' + str(wrk_output.get('req_max')) + ',' + str(
+        wrk_output.get('tot_requests')) + ',' + str(wrk_output.get('tot_duration')) + ',' + str(
+        wrk_output.get('read')) + ',' + str(wrk_output.get('err_connect')) + ',' + str(
+        wrk_output.get('err_read')) + ',' + str(wrk_output.get('err_write')) + ',' + str(
+        wrk_output.get('err_timeout')) + ',' + str(wrk_output.get('req_sec_tot')) + ',' + str(
+        wrk_output.get('transfer_sec'))
+
+
+def get_mib(size_str: str) -> Any:
     x = re.search("^(\d+\.*\d*)(\w+)$", size_str)
     if x is not None:
         size = float(x.group(1))
@@ -11,17 +24,17 @@ def get_bytes(size_str: str) -> Any:
         return size_str
 
     if suffix == 'b':
-        return size
+        return size/(1024 ** 2)
     elif suffix == 'kb' or suffix == 'kib':
-        return size * 1024
+        return size/(1024)
     elif suffix == 'mb' or suffix == 'mib':
-        return size * 1024 ** 2
+        return size
     elif suffix == 'gb' or suffix == 'gib':
-        return size * 1024 ** 3
+        return size * 1024
     elif suffix == 'tb' or suffix == 'tib':
-        return size * 1024 ** 3
+        return size * 1024 ** 2
     elif suffix == 'pb' or suffix == 'pib':
-        return size * 1024 ** 4
+        return size * 1024 ** 3
 
     return False
 
@@ -91,13 +104,13 @@ def parse_wrk_output(wrk_output: str) -> Dict[str, Any]:
         if x is not None:
             retval['tot_requests'] = get_number(x.group(1))
             retval['tot_duration'] = get_ms(x.group(2))
-            retval['read'] = get_bytes(x.group(3))
+            retval['read'] = get_mib(x.group(3))
         x = re.search("^Requests\/sec\:\s+(\d+\.*\d*).*$", line)
         if x is not None:
             retval['req_sec_tot'] = get_number(x.group(1))
         x = re.search("^Transfer\/sec\:\s+(\d+\.*\d*\w+).*$", line)
         if x is not None:
-            retval['transfer_sec'] = get_bytes(x.group(1))
+            retval['transfer_sec'] = get_mib(x.group(1))
         x = re.search(
             "^\s+Socket errors:\ connect (\d+\w*)\,\ read (\d+\w*)\,\ write\ (\d+\w*)\,\ timeout\ (\d+\w*).*$", line)
         if x is not None:
