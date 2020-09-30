c8cb775f2063da18860445694f099360ab31fa6c
Jörg Thalheim <joerg@thalheim.io>
2020-09-30 15:17:17 +0200

    re-do network optimization graphs

--- apps/nix/micro_bench_plots.py
+++ apps/nix/micro_bench_plots.py
@@ -8,102 +8,6 @@ import os
 from graph_utils import apply_aliases, change_width, column_alias
 
 
-def iperf_zerocopy_plot(dir: str, graphs: List[Any]) -> None:
-    df_all_on = pd.read_csv(
-        os.path.join(os.path.realpath(dir), "iperf-all-on.tsv"), sep="\t"
-    )
-
-    df_zcopy_off = pd.read_csv(
-        os.path.join(os.path.realpath(dir), "iperf-zerocopy-off.tsv"), sep="\t"
-    )
-
-    df_all_on = df_all_on[df_all_on["direction"] == "send"]
-    df_zcopy_off = df_zcopy_off[df_zcopy_off["direction"] == "send"]
-
-    df_all_on["iperf-throughput"] = df_all_on["bytes"] / df_all_on["seconds"] * 8 / 1e9
-    df_zcopy_off["iperf-throughput"] = (
-        df_zcopy_off["bytes"] / df_zcopy_off["seconds"] * 8 / 1e9
-    )
-
-    df_all_on["feature_dpdk"] = pd.Series(
-        ["dpdk-zerocopy"] * len(df_all_on.index), index=df_all_on.index
-    )
-    df_zcopy_off["feature_dpdk"] = pd.Series(
-        ["dpdk-copy"] * len(df_zcopy_off.index), index=df_zcopy_off.index
-    )
-
-    plot_df = pd.concat([df_all_on, df_zcopy_off], axis=0)
-    plot_df = plot_df.reset_index(drop=True)
-
-    g = catplot(
-        data=apply_aliases(plot_df),
-        x=column_alias("feature_dpdk"),
-        y=column_alias("iperf-throughput"),
-        kind="bar",
-        height=2.5,
-        legend=False,
-        ci=None,
-        color="black",
-        palette=None,
-    )
-
-    change_width(g.ax, 0.25)
-    g.ax.set_xlabel("")
-
-    g.ax.set_xticklabels(g.ax.get_xmajorticklabels(), fontsize=6)
-    g.ax.set_yticklabels(g.ax.get_ymajorticklabels(), fontsize=6)
-
-    graphs.append(g)
-
-
-def iperf_offload_plot(dir: str, graphs: List[Any]) -> None:
-    df_all_on = pd.read_csv(
-        os.path.join(os.path.realpath(dir), "iperf-all-on.tsv"), sep="\t"
-    )
-
-    df_zcopy_off = pd.read_csv(
-        os.path.join(os.path.realpath(dir), "iperf-offload-off.tsv"), sep="\t"
-    )
-
-    df_all_on = df_all_on[df_all_on["direction"] == "send"]
-    df_zcopy_off = df_zcopy_off[df_zcopy_off["direction"] == "send"]
-
-    df_all_on["iperf-throughput"] = df_all_on["bytes"] / df_all_on["seconds"] * 8 / 1e9
-    df_zcopy_off["iperf-throughput"] = (
-        df_zcopy_off["bytes"] / df_zcopy_off["seconds"] * 8 / 1e9
-    )
-
-    df_all_on["feature_dpdk"] = pd.Series(
-        ["dpdk-offload"] * len(df_all_on.index), index=df_all_on.index
-    )
-    df_zcopy_off["feature_dpdk"] = pd.Series(
-        ["dpdk-no-offload"] * len(df_zcopy_off.index), index=df_zcopy_off.index
-    )
-
-    plot_df = pd.concat([df_all_on, df_zcopy_off], axis=0)
-    plot_df = plot_df.reset_index(drop=True)
-
-    g = catplot(
-        data=apply_aliases(plot_df),
-        x=column_alias("feature_dpdk"),
-        y=column_alias("iperf-throughput"),
-        kind="bar",
-        height=2.5,
-        legend=False,
-        ci=None,
-        color="black",
-        palette=None,
-    )
-
-    change_width(g.ax, 0.25)
-    g.ax.set_xlabel("")
-
-    g.ax.set_xticklabels(g.ax.get_xmajorticklabels(), fontsize=6)
-    g.ax.set_yticklabels(g.ax.get_ymajorticklabels(), fontsize=6)
-
-    graphs.append(g)
-
-
 def preprocess_hdparm(df_col: pd.Series) -> Any:
     df_col = list(df_col.values)
     for i in range(len(df_col)):
@@ -264,6 +168,33 @@ def smp_plot(dir: str, graphs: List[Any]) -> None:
     graphs.append(g)
 
 
+def read_iperf(path: str, type: str) -> pd.DataFrame:
+    df = pd.read_csv(path, sep="\t")
+    df = df[df["direction"] == "send"]
+    df["iperf-throughput"] = df["bytes"] / df["seconds"] * 8 / 1e9
+    return df.assign(type=type)
+
+
+def network_optimization_plot(dir: str, graphs: List[Any]) -> None:
+    df_all = read_iperf(os.path.join(os.path.realpath(dir), "iperf-all-on.tsv"), "offloads+\nzerocopy")
+
+    df_offload = read_iperf(os.path.join(os.path.realpath(dir), "iperf-offload-off.tsv"), "no offloads")
+
+    df_zcopy = read_iperf(os.path.join(os.path.realpath(dir), "iperf-zerocopy-off.tsv"), "no zerocopy")
+    df = pd.concat([df_all, df_offload, df_zcopy])
+    g = catplot(
+        data=apply_aliases(df),
+        x=column_alias("type"),
+        y=column_alias("iperf-throughput"),
+        kind="bar",
+        height=2.5,
+        aspect=1.2,
+    )
+    change_width(g.ax, 0.25)
+    g.ax.set_xlabel('')
+    graphs.append(g)
+
+
 def main() -> None:
     if len(sys.argv) < 1:
         sys.exit(1)
@@ -272,12 +203,11 @@ def main() -> None:
     graph_names = []
 
     plot_func = {
-        "dpdk_zerocopy": iperf_zerocopy_plot,
-        "dpdk_offload": iperf_offload_plot,
         "spdk_zerocopy": hdparm_zerocopy_plot,
         "network_bs": network_bs_plot,
         "storage_bs": storage_bs_plot,
         "smp": smp_plot,
+        "network_optimization": network_optimization_plot
     }
 
     for name, pf in plot_func.items():
